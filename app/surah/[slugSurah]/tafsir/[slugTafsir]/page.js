8f6eee4a9d00564db367ead3642b545c489bdc15
wonderkid <mwahyuap2018@gmail.com>
2023-03-03 22:28:58 +0700

    fixing size of div tafsir on mobile screen

--- app/surah/[slugSurah]/tafsir/[slugTafsir]/page.js
+++ app/surah/[slugSurah]/tafsir/[slugTafsir]/page.js
@@ -21,12 +21,12 @@ export default async function Tafsir({ params }) {
   return (
     <div>
       {singleTafsirAyat.map((tafsir) => (
-        <div key={params.slugTafsir} className="rounded-md w-1/2 mx-auto mb-5">
+        <div key={params.slugTafsir} className="rounded-md w-5/6 md: w-2/4 lg:w-1/2 text-center mx-auto mb-5">
           <h1 className="font-semibold cursor-pointer text-lg text-center bg-emerald-700 my-5 text-white rounded-md inline-block p-3">
             Tafsir {params.slugSurah} ayat {params.slugTafsir}
           </h1>
           <div
-            className="space rounded-md border-emerald-700 border-2 p-3"
+            className="space text-left rounded-md border-emerald-700 border-2 p-3"
             dangerouslySetInnerHTML={{ __html: tafsir.teks }}
           />
         </div>
